0456dcc01b02e8c55b9adba21a258543e063f6ad
Gubitelson <119538200+Gubitelson@users.noreply.github.com>
2023-02-14 00:31:33 +0400

    небольшая переделка

--- zadanie/Program.cs
+++ zadanie/Program.cs
@@ -1,12 +1,15 @@
 ﻿Console.Clear();
 string[] StringArray;
 StringArray = Console.ReadLine().Split(new char[] { ',', ' ' });
+string m = "слова и цифры в массиве которые в тексте 3 и меньше символа"; 
 for (int i = 0; i < StringArray.Length; i++)
 {
 if (StringArray[i].Length <= 3)
     {
-        string m = StringArray[i];
-        Console.Write(m + ", " );
+        m = StringArray[i];
+       Console.Write( m + "," );
 
     }
+    
 }
+ 
