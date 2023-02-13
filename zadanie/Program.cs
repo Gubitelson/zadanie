ec6e59212b0bc7ef58a40e93a9009ec49eae5473
Gubitelson <119538200+Gubitelson@users.noreply.github.com>
2023-02-13 20:21:10 +0400

    программа доделана

--- zadanie/Program.cs
+++ zadanie/Program.cs
@@ -1,4 +1,12 @@
 ﻿Console.Clear();
 string[] StringArray;
 StringArray = Console.ReadLine().Split(new char[] { ',', ' ' });
+for (int i = 0; i < StringArray.Length; i++)
+{
+if (StringArray[i].Length <= 3)
+    {
+        string m = StringArray[i];
+        Console.Write(m + ", " );
 
+    }
+}
