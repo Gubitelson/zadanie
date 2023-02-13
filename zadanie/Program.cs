de47897e3647f9fe67e88a7c5c992cd0c76dd70c
Gubitelson <119538200+Gubitelson@users.noreply.github.com>
2023-02-14 00:41:14 +0400

    доделка и добавление блоксхемы

--- zadanie/Program.cs
+++ zadanie/Program.cs
@@ -1,13 +1,16 @@
 ﻿Console.Clear();
+Console.WriteLine("ВВедите строки и символы через пробел и запятую");
 string[] StringArray;
 StringArray = Console.ReadLine().Split(new char[] { ',', ' ' });
-string m = "слова и цифры в массиве которые в тексте 3 и меньше символа"; 
+string m ; 
+Console.WriteLine();
+Console.Write("Строки и числа которые 3 и меньше символов: " );
 for (int i = 0; i < StringArray.Length; i++)
 {
 if (StringArray[i].Length <= 3)
     {
         m = StringArray[i];
-       Console.Write( m + "," );
+       Console.Write( m + ", " );
 
     }
     
